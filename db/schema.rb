88fae16ad942a1f5d47c6098f3de61396c867e74
William Le <mrwillyle@gmail.com>
2015-11-25 13:46:18 -0500

    Added note column to resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123173053) do
+ActiveRecord::Schema.define(version: 20151125181724) do
 
   create_table "languages", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151123173053) do
     t.integer  "link_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "note"
   end
 
   create_table "searches", force: :cascade do |t|

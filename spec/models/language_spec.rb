edeee4918356b5be250e09df339e0630d2a84202
Shawn Patrick Doyle <shawnpatrickdoyle@gmail.com>
2015-11-27 09:42:54 -0500

    language validations added

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:valid_attributes){
+  	@valid_attributes = {
+  		name: "Ruby"
+  	}
+  }
+  describe "is invalid without a " do
+  	let(:language){Language.new(valid_attributes)}
+  	it "name" do
+  		language.name = nil
+  		expect(language).to be_invalid
+  	end
+  end
+  describe "duplicate to be invalid" do
+  	let(:language){Language.new(valid_attributes)}
+  	@duplicate = Language.new(name: "Ruby")
+  	it "name" do
+  		expect(@duplicate).to be_nil
+  	end
+  end
 end

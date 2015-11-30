7057609f8161e63dfd6070afb062f39a88f2da65
William Le <mrwillyle@gmail.com>
2015-11-30 10:43:53 -0500

    Close button is working too.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if @user
         session[:user_id] = @user.id
         flash[:notice] = "You have logged in successfully"
-        redirect_to dashboard_path(@user)
+        redirect_to dashboard_path
     else
       flash[:alert] = "Your information was incorrect."
       redirect_to sign_in_path
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( dashboard.js )
+Rails.application.config.assets.precompile += %w( search_link.js )
